df0b09e2298b73d0cbdfa67c6bcfd8ef958a2095
Minimal-Alexi <alexpop@metropolia.fi>
2025-04-03 13:50:26 +0300

    Preparing frontend localization.

--- src/main/java/org/metropolia/uidblocalization/JobController.java
+++ src/main/java/org/metropolia/uidblocalization/JobController.java
@@ -1,5 +1,22 @@
 package org.metropolia.uidblocalization;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
 public class JobController {
+    @FXML
+    private Label labelTitle;
+    @FXML
+    private ComboBox<String> comboBoxLanguage;
+    @FXML
+    private ListView<String> listViewJobs;
+    public void initialize() {
+
+    }
+    private void comboBoxInitializer(){
+
+    }
 
 }
